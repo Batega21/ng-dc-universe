c05944efdc55ac40a0354dc6c106a0a327859bb9
Gabriel Gomez <batega21@gmail.com>
2025-05-22 03:00:24 -0300

    hero not found search box error

--- src/app/shared/search-box/search-box.component.ts
+++ src/app/shared/search-box/search-box.component.ts
@@ -5,23 +5,17 @@ import {
   signal,
 } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-import {
-  debounceTime,
-  filter,
-  distinctUntilChanged,
-  tap,
-  switchMap,
-} from 'rxjs';
 import { HeroService } from '../../core/services/hero.service';
 import { LoggerService } from '../../core/services/logger.service';
 import { HeroesProvider } from '../../state/hero.store';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-search-box',
-  imports: [MatButtonModule, MatIconModule, ReactiveFormsModule],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, ReactiveFormsModule],
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.scss',
 })
@@ -46,40 +40,6 @@ export class SearchBoxComponent {
     }
   }
 
-  ngOnInit() {
-    this.searchBoxForm
-      .get('searchBox')
-      ?.valueChanges.pipe(
-        debounceTime(300),
-        filter((value) => (value ?? '').length > 2),
-        distinctUntilChanged(),
-        tap((value) => {
-          this.searchValue.update(() => value);
-          this._loggerService.log(value);
-        }),
-        switchMap((value) =>
-          this._heroesService.getHeroesByQueryParams(value as string)
-        )
-      )
-      .subscribe({
-        next: (response) => {
-          this.heroesQuery.update((heroes) => {
-            const existingHeroes = heroes.filter((hero) =>
-              response.map((h) => h.name).includes(hero)
-            );
-            if (existingHeroes.length > 0) return heroes;
-
-            const newHeroes = response.map((hero) => hero.name);
-            return [...heroes, ...newHeroes];
-          });
-        },
-        error: (error) => {
-          this._loggerService.error('Error fetching heroes', error);
-          this.onClearFilter();
-        },
-      });
-  }
-
   onClearFilter() {
     this.searchValue.update(() => '');
     this.heroesQuery.update(() => []);
@@ -89,6 +49,27 @@ export class SearchBoxComponent {
   onFilterChange(event: Event) {
     const input = event.target as HTMLInputElement;
     this.searchValue.update(() => input.value);
+
+    if (input.value.length >= 3) {
+      this._heroesService.getHeroesByQueryParams(input.value)
+        .subscribe({
+          next: (response) => {
+            this.heroesQuery.update((heroes) => {
+              const existingHeroes = heroes.filter((hero) =>
+                response.map((h) => h.name).includes(hero)
+              );
+              if (existingHeroes.length > 0) return heroes;
+
+              const newHeroes = response.map((hero) => hero.name);
+              return [...heroes, ...newHeroes];
+            });
+          },
+          error: (error) => {
+            this._loggerService.error('Error fetching heroes', error);
+            this.onClearFilter();
+          },
+        })
+    }
   }
 
   onSelectedHero(heroName: string) {
